5515bf66500c6b12f24444f5a2b111b1dfc1dc90
Himanshuparmar29 <159614851+Himanshuparmar29@users.noreply.github.com>
2024-03-08 11:58:53 +0000

    change

--- temp.js
+++ temp.js
@@ -1,1 +1,12 @@
-let name1="Himanshu";
+let btn=document.createElement("button");
+let body=document.querySelector("body");
+btn.style.backgroundColor="red";
+btn.style.color="white";
+btn.innerText="Click Me!";
+
+body.prepend(btn);
+
+let p=document.createElement("p");
+p.style.backgroundColor="red";
+p.style.color="white";
+p.setAttribute("class","")
